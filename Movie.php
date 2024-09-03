081d8097f06e4f55da6dd8e4fcefc0bab5e87b20
FraCuboni <fracuboni@gmail.com>
2024-09-03 16:47:23 +0200

    EXERCISE AND BONUS 1 COMPLETED

--- Movie.php
+++ Movie.php
@@ -1,1 +1,98 @@
 <?php
+class Movie
+{
+    // valori delle istanze (o oggetti)
+    public $title;
+    public $director;
+    public $genre;
+    public $actors = [];
+
+    // costruttore
+    public function __construct($title, $director, $genre, $actors = [])
+    {
+        $this->title = $title;
+        $this->director = $director;
+        $this->genre = $genre;
+        $this->actors = $actors;
+    }
+
+    // creo funzioni per ottenere info del film
+    public function getMovieTitle()
+    {
+        return $this->title;
+    }
+    public function getMovieDirector()
+    {
+        return $this->director;
+    }
+    public function getMovieGenre()
+    {
+        return $this->genre;
+    }
+    public function getMovieActors()
+    {
+        return $this->actors;
+    }
+}
+
+// creazione manuale di un istanza
+// $parasite = new Movie();
+// $parasite->title = 'Parasite';
+// $parasite->director = 'Bong Joon-ho';
+// $parasite->genre = 'Thriller';
+
+// creazione 'automatica' di un istanza con il costruttore
+
+// creo array di film per ciclare con [i]
+$moviesArray = [
+    // creazione 'automatica' di un istanza con il costruttore
+    $pulpFiction = new Movie(
+        'Pulp Fiction',
+        'Quentin Tarantino',
+        'Thriller',
+        ['John Travolta', 'Uma Thurman', 'Samuel L. Jackson']
+    ),
+    $aSpaceOdissey = new Movie(
+        '2001 - a space odissey',
+        'Stanley Kubrick',
+        'Sci-Fi',
+        ['Keir Dullea', 'Gary Lockwood', 'William Sylvester']
+    ),
+    $parasite = new Movie(
+        'Parasite',
+        'Bong Joon-ho',
+        'Thriller',
+        ['Song Kang-ho', 'Lee Sun-kyun', 'Cho Yeo-jeong']
+    ),
+    $goodFellas = new Movie(
+        'GoodFellas',
+        'Martin Scorsese',
+        'Thriller',
+        ['Ray Liotta', 'Robert De Niro', 'Joe Pesci']
+    ),
+    $theGrandBudapestHotel = new Movie(
+        'The Grand Budapest Hotel',
+        'Wes Anderson',
+        'Commedy',
+        ['Ralph Fiennes', 'F. Murray Abraham', 'Mathieu Amalric']
+    ),
+    $bigFish = new Movie(
+        'Big Fish',
+        'Tim Burton',
+        'Adventure',
+        ['Ewan McGregor', 'Albert Finney', 'Jessica Lange']
+    ),
+    $jurassicPark = new Movie(
+        'Jurassic Park',
+        'Steven Spielberg',
+        'Sci-Fi',
+        ['Sam Neill', 'Laura Dern', 'Jeff Goldblum']
+    ),
+
+    $nosferatu = new Movie(
+        'Nosferatu',
+        'F.W. Murnau',
+        'Horror',
+        ['Max Schreck', 'Gustav von Wangenheim', 'Greta Schroeder']
+    )
+];
--- index.php
+++ index.php
@@ -1,55 +1,7 @@
 <?php
+require_once __DIR__ . '/Movie.php';
 //classe
-class Movie
-{
-    // valori delle istanze (o oggetti)
-    public $title;
-    public $director;
-    public $genre;
 
-    // costruttore
-    public function __construct($title, $director, $genre)
-    {
-        $this->title = $title;
-        $this->director = $director;
-        $this->genre = $genre;
-    }
-
-    // creo funzioni per ottenere info del film
-    public function getMovieTitle()
-    {
-        return "Title: " . $this->title;
-    }
-    public function getMovieDirector()
-    {
-        return "Title: " . $this->director;
-    }
-    public function getMovieGenre()
-    {
-        return "Title: " . $this->genre;
-    }
-}
-
-// creazione manuale di un istanza
-// $parasite = new Movie();
-// $parasite->title = 'Parasite';
-// $parasite->director = 'Bong Joon-ho';
-// $parasite->genre = 'Thriller';
-
-// creazione 'automatica' di un istanza con il costruttore
-$goodFellas = new Movie('GoodFellas', 'Martin Scorsese', 'Thriller');
-$grandBudapestHotel = new Movie('Grand Budapes Hotel', 'Wes Anderson', 'Sci-Fi');
-$bigFish = new Movie('Big Fish', 'Tim Burton', 'Thriller');
-
-// creo array di film per ciclare con [i]
-$moviesArray = [
-    // creazione 'automatica' di un istanza con il costruttore
-    $pulpFiction = new Movie('Pulp Fiction', 'Quentin Tarantino', 'Thriller'),
-    $aSpaceOdissey = new Movie('2001 - a space odissey"', 'Stanley Kubrick', 'Sci-Fi'),
-    $parasite = new Movie('Parasite', 'Bong Joon-ho', 'Thriller')
-];
-var_dump($moviesArray);
-var_dump('qui invece il film', $bigFish)
 
 ?>
 
@@ -68,23 +20,31 @@ var_dump('qui invece il film', $bigFish)
 <body>
     <div class="container">
         <div class="row">
-            <div class="card" style="width: 18rem;">
-                <div class="card-header">
-                    Movie title
+            <!-- inizio foreach -->
+            <?php
+            foreach ($moviesArray as $movie):
+            ?>
+                <div class="card" style="width: 18rem;">
+                    <div class="card-header">
+                        <h5><?php echo $movie->title ?></h5>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item"><?php echo $movie->director ?></li>
+                        <li class="list-group-item"><?php echo $movie->genre ?></li>
+                        <li class="list-group-item">Main Actors:</li>
+                        <li class="list-group-item">
+                            <ul class="list-group list-group-flush">
+                                <?php
+                                foreach ($movie->actors as $actor):
+                                ?>
+                                    <li class="list-group-item"><?php echo $actor ?></li>
+                                <?php endforeach; ?>
+                            </ul>
+                        </li>
+                    </ul>
                 </div>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">Movie director</li>
-                    <li class="list-group-item">Movie genre</li>
-                    <li class="list-group-item">Actors</li>
-                    <li class="list-group-item">
-                        <ul class="list-group list-group-flush">
-                            <li class="list-group-item">Cras justo odio</li>
-                            <li class="list-group-item">Dapibus ac facilisis in</li>
-                            <li class="list-group-item">Morbi leo risus</li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
+                <!-- fine foreach -->
+            <?php endforeach; ?>
         </div>
     </div>
 
